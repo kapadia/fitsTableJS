64c7ea1c7f57ac1eeb4e4778b9f24d6f8b6bc36e
Amit Kapadia <amit@zooniverse.org>
2012-04-13 11:37:19 -0500

    example for gh-pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,31 @@
-console.log('This would be the main JS file.');
+$(document).ready(function () {
+  var xhr, ft, columns, i, j, row, field;
+  
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', "test/data/spec-0406-51869-0012.fits", true);
+  xhr.responseType = 'arraybuffer';
+  xhr.onload = function(evt) {
+    var ft = new FitsTable(xhr.response);
+
+    ft.readHeader();
+    ft.readColumns();
+
+    columns = "<tr>"
+    for (i = 0; i < ft.columns; i += 1)
+      columns += "<td>" + ft.columnNames[i] + "</td>";
+    columns += "</tr>";
+    $("#example-table").append(columns);
+
+    for (j = 0; j < ft.rows; j += 1) {
+      row = "<td>";
+      for (i = 0; i < ft.columns; i += 1) {
+        columnName = ft.columnNames[i];
+        field = ft.data[columnName][j];
+        row += "<td>" + field + "</td>";
+      }
+      row += "</td>"
+      $("#example-table tr:last").after(row);
+    }
+  };
+  xhr.send();
+});
